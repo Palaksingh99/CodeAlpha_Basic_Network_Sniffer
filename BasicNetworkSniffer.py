34e880e1d57c2919855c243c93e16cb563f3101c
PALAK SINGH <palaksingh7361@gmail.com>
2024-07-25 11:44:51 +0530

    Update BasicNetworkSniffer.py

--- BasicNetworkSniffer.py
+++ BasicNetworkSniffer.py
@@ -1,24 +1,24 @@
 from scapy.all import sniff, Ether, IP, TCP, UDP
 
-def packet_callback(packet):
-    if Ether in packet:        
+def packet_callback(packet):       #Function to handle each packet
+    if Ether in packet:            # Check if the packet has an Ethernet layer
         eth_layer = packet[Ether]
         print(f"Ethernet Frame: {eth_layer.src} -> {eth_layer.dst} (Type: {eth_layer.type})")
     
-    if IP in packet:           
-        ip_layer = packet[IP]
+    if IP in packet:               # Check if the packet has an IP layer
+        ip_layer = packet[IP] 
         print(f"IP Packet: {ip_layer.src} -> {ip_layer.dst} (Protocol: {ip_layer.proto})")
 
-        if TCP in packet:
+        if TCP in packet:          # Check if the packet has an TCP layer 
             tcp_sport = packet[TCP].sport
             tcp_dport = packet[TCP].dport
             print(f"TCP Packet: {ip_layer.src}:{tcp_sport} -> {ip_layer.dst}:{tcp_dport}")
 
-        elif UDP in packet:
+        elif UDP in packet:        # Check if the packet has an UDP layer
             udp_sport = packet[UDP].sport
             udp_dport = packet[UDP].dport
             print(f"UDP Packet: {ip_layer.src}:{udp_sport} -> {ip_layer.dst}:{udp_dport}")
 
     print()
 
-sniff(prn=packet_callback,store=False)
+sniff(prn=packet_callback,store=False) # Start sniffing
